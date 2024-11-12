776cb89737e3ab366653b7e62a02ab2502096b39
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2024-11-12 17:50:04 -0500

    ejercicio 10

---
+++ 10-area-cuadrado.ts
@@ -0,0 +1,8 @@
+// Mostrar el área de un cuadrado
+
+function areaCuadrado(lado: number){
+    const calcular: number = lado * lado
+    return `El area de un cuadrado son ${calcular} metros cuadrados`
+}
+
+console.log(areaCuadrado(12))
