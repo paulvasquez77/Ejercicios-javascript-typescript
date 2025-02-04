31328ee740258d679b53ee8f02ca622e1e38d832
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2025-02-04 18:47:14 -0500

    Ejercicio 61

---
+++ 61-sum-digitos.ts
@@ -0,0 +1,9 @@
+// Leer un número entero de dos dígitos y determinar a cuánto es igual la suma de sus dígitos.
+
+function sumDigitos(num: number) {
+    const newDigitos = num.toString().split("")
+    const suma = Number(newDigitos[0]) + Number(newDigitos[1])
+    return `La suma de los digitos es ${suma}`
+}
+
+console.log(sumDigitos(34))
