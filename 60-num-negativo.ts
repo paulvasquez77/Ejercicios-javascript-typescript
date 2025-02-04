ee7868e19d65baa5f39c90913b5a6eeef92e9b2e
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2025-02-04 18:43:18 -0500

    Ejercicio 60

---
+++ 60-num-negativo.ts
@@ -0,0 +1,7 @@
+// Leer un número entero y determinar si es negativo.
+
+function numNegativo(num: number) {
+    return num < 0 ? "El numero es negativo" : "El numero es positivo"
+ }
+
+console.log(numNegativo(-1))
