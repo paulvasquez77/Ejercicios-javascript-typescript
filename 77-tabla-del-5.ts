e32421c9db4f60a5dc2cea20f9c7d1eaebfd1460
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2025-02-12 21:18:36 -0500

    Ejercicio 77

---
+++ 77-tabla-del-5.ts
@@ -0,0 +1,9 @@
+// Mostrar en pantalla la tabla de multiplicar del número 5.
+
+function tablaDel5() {
+    for (let i = 1; i <= 10; i++) {
+         console.log(`${5} x ${i} = ${5 * i}`)
+    }
+}
+
+console.log(tablaDel5())
